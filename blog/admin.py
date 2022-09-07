6bf4c22dc0143ad19267407b9c2521d1b6676e64
KanchanCS <imkanchan7422@gmail.com>
2022-09-07 15:24:31 +0530

    add code

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Post , Category 
+from .models import Post , Category ,Banner, About,Contact
 # Register your mod
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Banner)
+admin.site.register(About)
+admin.site.register(Contact)
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 , redirect
 from .models import *
-# Create your views here.
+from .form import ContactForm
 
+# Create your views here.
 def home(request):
-    recent_post = Post.objects.all().order_by('-create_date')[:6]
+    all_post = Post.objects.all().order_by('-create_date')
+    banner = Banner.objects.all()
+    recent_post = all_post[:6]
     part_one  = recent_post[:3]
     part_two = recent_post[3:6]
-    last = Post.objects.last()
+    last = recent_post[0]
     context = {
         "part_one":part_one,
         "part_two":part_two,
         "recent_post":recent_post,
-        "last":last,
+        "all_post":all_post,
+        "last":last, 
+        "banner":banner,
     }
     return render(request, 'home.html', context)
 
 def category(request):
     category = Category.objects.all()  
+
     return render (request, 'category.html',{'category':category})
 
-                 
+def post_details(request, id):
+    single_post = get_object_or_404(Post, id = id)
+    context = {
+        "single_post":single_post,
+    }
+    template_name = 'post_detail.html'
+    return render(request, 'post_detail.html', context)
+
+
+def about(request):
+    about = About.objects.all().order_by('-id')[:4]
+
+    return render(request, 'about.html',{'about':about})  
+                       
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("blog:home")
+    else:
+        form = Contact()
+    context = {'form': form}
+    return render(request, 'contact.html', context)
+
+# def contact(request): 
+#     if request.method == 'POST':
+#             name = request.POST('name')
+#             email= request.POST('email')
+#             sub = request.POSTt('sub')
+#             message = request.POST('message')
+             
+#             app=Contact(
+#             name = name,
+#             email= email,
+#             sub = sub,
+#             message = message)
+#             app.save() 
+#             messages.add_message(request, messages.INFO, 'Send message succesfully.') 
+#             return redirect('home.html') 
+#     else:
+#         return render(request, 'contact.html')
